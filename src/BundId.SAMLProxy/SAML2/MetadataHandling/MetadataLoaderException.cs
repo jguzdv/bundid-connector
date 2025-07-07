05a9e87bbd3ce3a4ab849670f7c71709012376ba
Ottenhus, Thomas <Ottenhus, Thomas>
2025-07-07 12:16:27 +0200

    Copy certificate and metadata handling

---
+++ src/BundId.SAMLProxy/SAML2/MetadataHandling/MetadataLoaderException.cs
@@ -0,0 +1,16 @@
+namespace JGUZDV.BundId.SAMLProxy.SAML2.MetadataHandling;
+
+/// <summary>
+/// Exception to indicate that metadata loading (fetching an EntityDescriptor) went wrong.
+/// </summary>
+[Serializable]
+internal class MetadataLoaderException : Exception
+{
+    public MetadataLoaderException(string? message) : base(message)
+    {
+    }
+
+    public MetadataLoaderException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
